303a7cab2e38f74e66b8b77fefac9dc714d87f10
gon0512 <layonfigueiroa@gmail.com>
2020-10-26 13:16:55 -0300

    adicao da pagina de boas vindas e outras pequenas alteracoes

--- js/script.js
+++ js/script.js
@@ -1,42 +1,29 @@
 //Usando localStorage para armazenar dados da página
 
+localStorage.setItem('usuario', 'layonfigueiroa');
+localStorage.setItem('senha', '12345');
+
 var logon, logout;
 
 function efetuarLogin(){
 	var login = document.getElementById("inputLogin").value;
 	var senha = document.getElementById("inputSenha").value;
 	var user = localStorage.getItem("usuario");
-	var password = localStorage.getItem("senha");
+	var pass = localStorage.getItem("senha");
 
-	if(user === login && password === senha){
-		logon = window.open('index.html', '_self');
+	if(login != '' && senha != ''){
+		if(user === login && pass === senha){
+			logon = window.open('index.html', '_self');
+		}
+		else{
+			alert("Erro ao efetuar login!");
+			logon = window.open('paginaLogin.html', '_self');
+		}
 	}
 	else{
-		alert("Erro ao efetuar login!");
-		logon = window.open('paginaLogin.html', '_self');
+		alert("Digite um login e senha válidos!");
 	}
 }
-
-function efetuarLogout(){
-	logout = window.open('paginaLogin.html', '_self');
-	logout.close();
-}
-
-function registrarUsuario(){
-	var user = document.getElementById("nome").value;
-	if(user != null){
-		localStorage.setItem("login", user);
-	}
-	else{
-		alert("digite um nome válido!");
-	}
-}
-
 var botaoLogin = document.getElementById("botaoEntrar");
 botaoLogin.addEventListener("click", efetuarLogin);
 
-var botaoLogout = document.getElementById("logout");
-botaoLogout.addEventListener("click", efetuarLogout);
-
-var botaoRegistrar = document.getElementById("botao");
-botaoRegistrar.addEventListener("click", registrarUsuario);
